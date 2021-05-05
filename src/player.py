4ab0777e4c46d030030b7a90b6fba64f1f07f543
Allison Usher <allyjay317@gmail.com>
2021-05-04 19:09:24 -0700

    created player class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,17 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, name, start):
+        self.name = name
+        self.room = start
+
+    def __str__(self):
+        return self.room.__str__()
+
+    def move(self, dir):
+        if hasattr(self.room, dir):
+            self.room = getattr(self.room, dir)
+            return self.room
+        else:
+            return "I can't move there"
