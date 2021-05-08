6e47dc19e67b0a5f6f2286807d2cd1da541c1099
Allison Usher <allyjay317@gmail.com>
2021-05-07 19:39:09 -0700

    added to room ability to add/remove item from room

--- src/room.py
+++ src/room.py
@@ -5,6 +5,28 @@ class Room:
     def __init__(self, name, dscr):
         self.name = name
         self.dscr = dscr
+        self.__items__ = []
 
     def __str__(self):
-        return self.name + "\n" + self.dscr
+        output = self.name + "\n" + self.dscr
+        if len(self.__items__):
+            output += "\nIn this room, you can see: "
+            for item in self.__items__:
+                output += item.name + ", "
+            output = output[:-2]
+        return output
+
+    def removeItem(self, name):
+        new_items = []
+        for i, item in enumerate(self.__items__):
+            if not str.lower(item.name) == name:
+                new_items.append(item)
+            else:
+                new_items = new_items + self.__items__[i+1:]
+                self.__items__ = new_items
+                return item
+        self.__items__ = new_items
+        return None
+
+    def addItem(self, item):
+        self.__items__.append(item)
